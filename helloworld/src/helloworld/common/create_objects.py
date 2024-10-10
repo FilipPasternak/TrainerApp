f15dc217640b41b5b5ef147bc7d812a3495105e9
FilipPasternak <fpastern@student.agh.edu.pl>
2024-10-10 19:25:11 +0200

    Structure for goal page

--- helloworld/src/helloworld/common/create_objects.py
+++ helloworld/src/helloworld/common/create_objects.py
@@ -2,26 +2,26 @@ import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 
-def create_button(box, action, label, style) -> None:
+def create_button(box, action, label, style=Pack(padding=5)) -> None:
     button = toga.Button(label, on_press=action, style=style)
     box.add(button)
     return button
 
-def create_text_box(box, style):
+def create_text_box(box, style=Pack(padding=5)):
     ti = toga.TextInput(style=style)
     box.add(ti)
     return ti
 
-def create_label(box, text, style) -> None:
+def create_label(box, text, style=Pack(padding=5)) -> None:
     box.add(toga.Label(text=text, style=style))
 
-def create_switch(box, action, label, style, add_to_box=True) -> None:
+def create_switch(box, action, label, style=Pack(padding=5), add_to_box=True) -> None:
     sw = toga.Switch(text=label, on_change=action, style=style)
     if add_to_box:
         box.add(sw)
     return sw
 
-def create_selection(box, options, on_select):
-    sel = toga.Selection(items=options, on_select=on_select)
+def create_selection(box, options, on_select, style=Pack(padding=5)):
+    sel = toga.Selection(items=options, on_select=on_select, style=style)
     box.add(sel)
     return sel
--- helloworld/src/helloworld/features/pages/build_user_goal_page.py
+++ helloworld/src/helloworld/features/pages/build_user_goal_page.py
@@ -1,6 +1,7 @@
 import toga
 from toga.style import Pack
-from helloworld.common.create_objects import create_button, create_label
+from toga.style.pack import COLUMN, ROW
+from helloworld.common.create_objects import create_button, create_label, create_switch, create_selection
 from helloworld.common.handlers.gpt_handler import GptClient
 
 class UserGoalPage:
@@ -13,13 +14,45 @@ class UserGoalPage:
     def startup(self):
         import helloworld.common.actions as actions
 
-        main_box = toga.Box(style=Pack(direction='column'))
+        main_box = toga.Box(style=Pack(direction=COLUMN, padding=10))
 
-        create_label(box=main_box, text=self.gpt.chat_test(), style=Pack(padding=5))
+        experience_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        create_label(box=experience_box,
+                     text='Experience:')
+        _ = create_selection(box=experience_box,
+                             options=['Beginner', 'Worked out before', 'Working out regularly', 'Expert'],
+                             on_select=self.on_select)
 
-        create_button(box=main_box,
-                      action=actions.go_to_dev_page,
-                      label='Next',
-                      style=Pack(padding=5))
+        goal_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        create_label(box=goal_box,
+                     text='Your goal:')
+        _ = create_selection(box=goal_box,
+                             options=['Gain muscle', 'Lose weight', 'Healthy lifestyle'],
+                             on_select=self.on_select)
 
+        intensity_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        create_label(box=intensity_box,
+                     text='Training intensity:')
+        _ = create_selection(box=intensity_box,
+                             options=['Relaxing', 'Medium', 'Intense'],
+                             on_select=self.on_select)
+
+        frequency_box1 = toga.Box(style=Pack(direction=COLUMN, padding=5))
+        create_label(box=frequency_box1,
+                     text='Training frequency:')
+        frequency_box2 = toga.Box(style=Pack(direction=ROW, padding=5))
+        for day in ['Mon', 'Tue', 'Wed', 'Th', 'Fri', 'Sat', 'Sun']:
+            create_button(box=frequency_box2, action=self.button_push, label=day)
+
+        main_box.add(experience_box)
+        main_box.add(goal_box)
+        main_box.add(intensity_box)
+        main_box.add(frequency_box1)
+        main_box.add(frequency_box2)
         self.main_window.content = main_box
+
+    def on_select(self):
+        pass
+
+    def button_push(self):
+        pass
